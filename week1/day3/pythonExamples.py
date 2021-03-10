f0a8213783ef57be8bae4eebf0d8dc49946257a4
Whitney Svoboda <whitney.svoboda@gmail.com>
2021-03-10 10:32:03 -0600

    Makes Python file

---
+++ week1/day3/pythonExamples.py
@@ -0,0 +1,21 @@
+greeting = "Hello Class"
+classSize = 23
+floatExample = 2.323
+sumOfNumbers = 1 + 1
+variableName = True
+# [1, 2, 3]
+# ["string", "anotherString", "lastString"]
+# [True, False, True]
+# ["string", 1, 5.3, "strong"]
+# ["list, [1, 2, 3]]
+# ["list", ["lost"]]
+# {key: value}
+# {"favoriteColor": "Pink"}
+
+print("Hello World")
+
+print("Whitney | Grapevine, TX")
+
+print(classSize)
+
+somethingToCallIt = "This is a cool variable"
