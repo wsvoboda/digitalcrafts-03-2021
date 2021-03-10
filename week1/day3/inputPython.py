7f9d47454e38cbd81afc59e6499edba586d474b3
Whitney Svoboda <whitney.svoboda@gmail.com>
2021-03-10 11:52:52 -0600

    Created Python function with inputs

---
+++ week1/day3/inputPython.py
@@ -0,0 +1,4 @@
+nameOfUser = input("What is your first name? ")
+lastNameOfUser = input("What is your last name? ")
+print("Hello, %s, welcome to Python." % nameOfUser) #Usage of %s is called an f string, you can use %d if you want to insert an integer
+print("Hello, %s %s, welcome to Python." % (nameOfUser, lastNameOfUser)) #Usage of %s is called an f string, you can use %d if you want to insert an integer
