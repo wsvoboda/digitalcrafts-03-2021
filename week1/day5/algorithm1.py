37ba9bdf509e09b9fa526fa09970ce1d116ab5a2
Whitney Svoboda <whitney.svoboda@gmail.com>
2021-03-12 10:12:58 -0600

    Figures out FizzBuzz algorithm

--- week1/day5/algorithm1.py
+++ week1/day5/algorithm1.py
@@ -1,1 +1,11 @@
-print(range(1:100))
+listOfNumbers = list(range(1, 101))
+
+for n, i in enumerate(listOfNumbers):
+    if (i % 3 == 0) and (i % 5 == 0):
+        listOfNumbers[n] = "FizzBuzz"
+    elif i % 5 == 0:
+        listOfNumbers[n] = "Buzz"
+    elif (i % 3 == 0):
+        listOfNumbers[n] = "Fizz"
+
+print(listOfNumbers)
