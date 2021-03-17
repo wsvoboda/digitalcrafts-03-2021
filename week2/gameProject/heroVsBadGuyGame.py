35617755fc5a932538ddd334172902d51b8fc1d6
Whitney Svoboda <whitney.svoboda@gmail.com>
2021-03-17 18:02:49 -0500

    final updates to game for today

--- week2/gameProject/heroVsBadGuyGame.py
+++ week2/gameProject/heroVsBadGuyGame.py
@@ -28,7 +28,7 @@
 
 # show name of game
 # choose character
-# you have chosen "CHARACTER"! You have 2 berries and 2 asprin in your inventory. prepare yourself for battle again ENEMY
+# you have chosen "CHARACTER"! You have 2 berries and 2 asprin in your inventory. prepare yourself for battle against ENEMY
 
 # second menu
 
@@ -38,7 +38,7 @@
 #     default random amount 0-10
 #     if they eat 1 berry - random amount 11-20
 #     if they eat 2 berries - random amount 21-30
-# eat asprin (to heal - gives back 10)
+# eat aspirin (to heal - gives back 10)
 # eat berry (to get strength)
 # run away (ends game)
 
@@ -52,7 +52,7 @@ class GoodGuy:
         self.name = name
         self.power = power
         self.defense = defense
-        self.life = life
+        self.life = life # "life points"
     
     def __str__(self):
         return f"""
@@ -68,11 +68,12 @@ class GoodGuy:
     def fightBaddieWithOneBerry(self):
         pass
 
-    def fightBaddieWithOneBerry(self):
+    def fightBaddieWithTwoBerry(self):
         pass
 
-    def healthYourself(self):
+    def healYourself(self): # working
         self.life += 10 
+        return print(f"{self.name} chose to take an asprin! {self.name} now has {self.life} life points.")
 
 class BadGuy:
     def __init__(self, name, power, defense, life):
@@ -92,3 +93,51 @@ class BadGuy:
         Defense: {self.defense}
         """
     
+mango = GoodGuy("Mango", 50, 50, 50)
+peach = GoodGuy("Peach", 40, 60, 50)
+cherry = GoodGuy("Cherry", 60, 40, 50)
+
+peanut = BadGuy("Peanut", 10, 10, 50)
+cashew = BadGuy("Cashew", 50, 40, 50)
+almond = BadGuy("Almond", 40, 60, 50)
+
+def firstMessage():
+    print("""
+    Welcome to the battle!
+    Your opponent will be chosen at random.
+    Please choose your warrior!""")
+    print(mango, peach, cherry)
+    choice = input("Type m for Mango, p for Peach, or c for Cherry: ")
+    if choice == "m":
+        print("\nYou chose Mango! Your opponent is Peanut!\n\nYou have 2 berries and 2 aspirin in your inventory.\nUse your turn to take a berry to gain strength and inflict more damage.\nUse your turn to take an aspirin to heal yourself and gain life points.")
+    elif choice == "p":
+        print("\nYou chose Peach! Your opponent is Peanut!\n\nYou have 2 berries and 2 aspirin in your inventory.\nUse your turn to take a berry to gain strength and inflict more damage.\nUse your turn to take an aspirin to heal yourself and gain life points.")
+    elif choice == "c":
+        print("\nYou chose Cherry! Your opponent is Peanut!\n\nYou have 2 berries and 2 aspirin in your inventory.\nUse your turn to take a berry to gain strength and inflict more damage.\nUse your turn to take an aspirin to heal yourself and gain life points.")
+    else:
+        print("I don't have a character by that name. Please choose a character.")
+
+firstMessage()
+
+def fightMenu():
+    print(f"""
+    You are battling! Choose one of the following options:
+    1. Fight!
+    2. Eat a berry
+    3. Take an aspirin
+    4. Do nothing
+    5. Run away
+    """)
+    battleChoice = int(input("What will you do? "))
+    if battleChoice == 1:
+        pass # do damage to enemy
+    elif battleChoice == 2:
+        pass # increase strength
+    elif battleChoice == 3:
+        pass # increase life points
+    elif battleChoice == 4: 
+        pass # do nothing, loop around
+    elif battleChoice == 5:
+        print("\nGood game! Bye bye ")
+
+fightMenu()
