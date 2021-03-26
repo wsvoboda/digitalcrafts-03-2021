67c5a05e3d3f7c6960a2318c09a6a2ff6b01e259
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-03-26 16:11:50 -0500

    adds instructor solution to algo

--- week3/day5/chessboardalg.py
+++ week3/day5/chessboardalg.py
@@ -14,46 +14,59 @@ white = " "
 
 # solution with print functions
 
-# def printWhiteStart():
-#     return print(white, black, white, black, white, black, white, black)
+def printWhiteStart():
+    return print(white, black, white, black, white, black, white, black)
 
-# def printBlackStart():
-#     return print(black, white, black, white, black, white, black, white)
+def printBlackStart():
+    return print(black, white, black, white, black, white, black, white)
 
 # printWhiteStart()
 # printBlackStart()
 
 # works but seems too easy
 
-# count = 0
+count = 0
 
-# while count < 4:
-#     printWhiteStart()
-#     printBlackStart()
-#     count += 1
+while count < 4:
+    printWhiteStart()
+    printBlackStart()
+    count += 1
 
-# solution with lists
+# my solution with lists
 
-# blackStartList = []
-# whiteStartList = []
+blackStartList = []
+whiteStartList = []
 
-# while len(blackStartList) < 8:
-#     blackStartList.append(black)
-#     blackStartList.append(white)
+while len(blackStartList) < 8:
+    blackStartList.append(black)
+    blackStartList.append(white)
 
-# while len(whiteStartList) < 8:
-#     whiteStartList.append(white)
-#     whiteStartList.append(black)
+while len(whiteStartList) < 8:
+    whiteStartList.append(white)
+    whiteStartList.append(black)
 
-# def printBoth():
-#     counter = 0
-#     while counter < 4:
-#         print(blackStartList)
-#         print(whiteStartList)
-#         counter += 1
+def printBoth():
+    counter = 0
+    while counter < 4:
+        print(blackStartList)
+        print(whiteStartList)
+        counter += 1
 
-# printBoth()
+printBoth()
 
+# solution from instructor
 
-
-
+def chessboard():
+   tile = False
+   board = ''
+   for i in range(64):
+      if (i != 0 and i % 8 == 0) :
+         board += "\n"
+         tile = not tile
+      if tile == True:
+         board += " "
+      else:
+         board += "#"
+      tile = not tile
+   return board
+print(chessboard())
