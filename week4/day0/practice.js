378a796a9732fa8ff73d55772a81b2bc3504da21
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-03-29 15:36:36 -0500

    uploads finished dantes exercise

--- week4/day0/practice.js
+++ week4/day0/practice.js
@@ -1,101 +1,111 @@
-// notes from the reading
+// // notes from the reading
 
-function sum(x, y) {
-    return console.log(x + y);
-  }
+// function sum(x, y) {
+//     return console.log(x + y);
+//   }
 
-sum(5,7)
+// sum(5,7)
 
-var sumOfNumbers = sum(5, 7);
-console.log(sum(5, 7));
+// var sumOfNumbers = sum(5, 7);
+// console.log(sum(5, 7));
 
-var x;
-for (x = 0; x < 10; x++) {
-  console.log(x);
-}
+// var x;
+// for (x = 0; x < 10; x++) {
+//   console.log(x);
+// }
 
-// madlib function
+// // madlib function
 
-function madlib(name, subject) {
-    return console.log(`${name}'s favorite subject in school is ${subject}.`);
-}
+// function madlib(name, subject) {
+//     return console.log(`${name}'s favorite subject in school is ${subject}.`);
+// }
 
-madlib('Anushka', 'art')
-madlib('Whitney', 'biology')
+// madlib('Anushka', 'art')
+// madlib('Whitney', 'biology')
 
+// // tip calculator that gives tip amount only
 
-// tip calculator that gives tip amount only 
+// function tipAmount(amount, serviceLevel) {
+//     if (serviceLevel == 'good') {
+//         tip = amount * 0.20;
+//         return tip;
+//     } else if (serviceLevel == 'fair') {
+//         tip = amount * 0.15;
+//         return tip;
+//     } else if (serviceLevel == 'bad') {
+//         tip = amount * 0.10;
+//         return tip;
+//     } else {
+//         return "Please give an accurate service level."
+//     }
+//     }
 
-function tipAmount(amount, serviceLevel) {
-    if (serviceLevel == 'good') {
-        tip = amount * 0.20;
-        return tip;
-    } else if (serviceLevel == 'fair') {
-        tip = amount * 0.15;
-        return tip;
-    } else if (serviceLevel == 'bad') {
-        tip = amount * 0.10;
-        return tip;
-    } else {
-        return "Please give an accurate service level."
-    }
-    }
+// console.log(tipAmount(100, 'good'))
 
-console.log(tipAmount(100, 'good'))
+// // gives total bill amount after adding in tip
 
-// gives total bill amount after adding in tip 
+// function tipAmount(amount, serviceLevel) {
+//     if (serviceLevel == 'good') {
+//         tip = amount * 1.20;
+//         return tip;
+//     } else if (serviceLevel == 'fair') {
+//         tip = amount * 1.15;
+//         return tip;
+//     } else if (serviceLevel == 'bad') {
+//         tip = amount * 1.10;
+//         return tip;
+//     } else {
+//         return "Please give an accurate service level."
+//     }
+//     }
 
-function tipAmount(amount, serviceLevel) {
-    if (serviceLevel == 'good') {
-        tip = amount * 1.20;
-        return tip;
-    } else if (serviceLevel == 'fair') {
-        tip = amount * 1.15;
-        return tip;
-    } else if (serviceLevel == 'bad') {
-        tip = amount * 1.10;
-        return tip;
-    } else {
-        return "Please give an accurate service level."
-    }
-    }
+// console.log(tipAmount(100, 'good'))
 
-console.log(tipAmount(100, 'good'))
+// // function to print numbers between x and y using a for loop and while loop
 
-// function to print numbers between x and y using a for loop and while loop
+// function printNumbers(x, y) {
+//     for (x = x; x <= y; x ++) {
+//         console.log(x)
+//     }
+// }
 
-function printNumbers(x, y) {
-    for (x = x; x <= y; x ++) {
-        console.log(x)
-    }
-}
+// printNumbers(1, 10)
 
-printNumbers(1, 10)
+// function printNumbers(x, y) {
+//     while (x <= y) {
+//         console.log(x);
+//         x++;
+//     }
+// }
 
-function printNumbers(x, y) {
-    while (x <= y) {
-        console.log(x);
-        x++;
-    }
-}
+// printNumbers(1, 10)
 
-printNumbers(1, 10)
+// function printSquare(sides) {
+//     while (x <= sides) {
+//         console.log(x * "8")
+//     }
+// }
 
-function printSquare(sides) {
-    while (x <= sides) {
-        console.log(x * "8")
-    }
-}
+// // print a square of asterisks
 
-// print a square of asterisks 
+// function printSquare(sides) {
+//     var star = "*";
+//     var i = 0;
 
-function printSquare(sides) {
-    var star = "*";
-    var i = 0;
+//         while(i++ < sides){
+//         console.log(star.repeat(sides));
+//     }
+// }
 
-        while(i++ < sides){
-        console.log(star.repeat(sides));
-    }
-}
+// printSquare(5)
 
-printSquare(5)
+// function printBanner(wordsToPrint) {
+//   let border = "-";
+//   return console.log(`
+//     ${border.repeat(wordsToPrint.length)}
+//     ${wordsToPrint}
+//     ${border.repeat(wordsToPrint.length)}
+//     `);
+// }
+
+// printBanner("- Welcome to DigitalCrafts -");
