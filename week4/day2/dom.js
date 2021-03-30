49bb5a1bd4c286b2962ddec5f87fb239f1c15173
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-03-30 11:07:45 -0500

    uploading morning notes

---
+++ week4/day2/dom.js
@@ -0,0 +1,22 @@
+// DOM - Document Object Model
+
+// can access things in the document object using dot notation
+
+// const document = {
+//   html: {
+//     head: {
+//       title: "",
+//     },
+//     body: {
+//       h1: "heading",
+//       a: "links",
+//     },
+//   },
+// };
+
+// BOM - Browser Object Model ~ enter "window" in console in inspect on webpage
+// window.history
+// window.screen - tells available height, pixels, etc
+// window.location - tells your location - host name, path, href, etc
+
+// put script tag at the bottom so the page fully loads first then the scripts are enabled at the end. JavaScript is a top-down language.
