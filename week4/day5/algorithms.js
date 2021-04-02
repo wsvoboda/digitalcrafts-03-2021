e5c0d38d9de8eef3350c5a327ceb45257423e1cc
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-02 10:49:15 -0500

    cleaned up first algorithm

--- week4/day5/algorithms.js
+++ week4/day5/algorithms.js
@@ -1,91 +1,85 @@
 //  want a function to take string and reverse it
 
 const string = "hi there";
+const string1 = "hello";
+const string3 = "hello hello hellooooo";
 
-console.log(string);
+const stringReversal = (str) => {
+  const stringToList = str.split("");
 
-const stringToList = string.split("");
-
-console.log(string.split("")); // testing
-
-const reverseArray = (input) => {
   var returnArray = [];
-  for (var i = input.length - 1; i >= 0; i--) {
-    returnArray.push(input[i]);
+  for (var i = str.length - 1; i >= 0; i--) {
+    returnArray.push(str[i]);
   }
-  return returnArray;
+  const completedReverseString = returnArray.join("");
+
+  return completedReverseString;
 };
 
-console.log(reverseArray(stringToList)); // testing
-
-const newReversedArray = reverseArray(stringToList);
-
-console.log(newReversedArray); // testing
-
-const completedReverseString = newReversedArray.join("");
-
-console.log(completedReverseString);
+console.log(stringReversal(string));
+console.log(stringReversal(string1));
+console.log(stringReversal(string3));
 
 //
 
 // take a number and reverse it but keep decimals and negative sign
 
-const number = -756.65;
+// const number = 0076;
 
-const changeSign = () => {
-  if (number < 0) {
-    const noSignNumber = number * -1;
-    return noSignNumber;
-  } else {
-    const noSignNumber = number;
-    return noSignNumber;
-  }
-};
+// const changeSign = () => {
+//   if (number < 0) {
+//     const noSignNumber = number * -1;
+//     return noSignNumber;
+//   } else {
+//     const noSignNumber = number;
+//     return noSignNumber;
+//   }
+// };
 
-const noSignNumber = changeSign(number);
+// const noSignNumber = changeSign(number);
 
-console.log(noSignNumber); // works
+// console.log(noSignNumber); // works
 
-const numberAsString = noSignNumber + "";
+// const numberAsString = noSignNumber + "";
 
-// console.log(numberAsString);
+// // console.log(numberAsString);
 
-// console.log(typeof numberAsString); // confirms it changes it to a string
+// // console.log(typeof numberAsString); // confirms it changes it to a string
 
-const arrayOfNums = numberAsString.split("");
+// const arrayOfNums = numberAsString.split("");
 
-console.log(arrayOfNums);
+// console.log(arrayOfNums);
 
-const reverseArray = (input) => {
-  var returnArray = [];
-  for (var i = input.length - 1; i >= 0; i--) {
-    returnArray.push(input[i]);
-  }
-  return returnArray;
-};
+// const reverseArray = (input) => {
+//   var returnArray = [];
+//   for (var i = input.length - 1; i >= 0; i--) {
+//     returnArray.push(input[i]);
+//   }
+//   return returnArray;
+// };
 
-const newReversedArray = reverseArray(arrayOfNums);
+// const newReversedArray = reverseArray(arrayOfNums);
 
-console.log(newReversedArray); // testing
+// console.log(newReversedArray); // testing
 
-const arrayToString = newReversedArray.join("");
+// const arrayToString = newReversedArray.join("");
 
-const noZeros = arrayToString.replace(/^0+|0+$/g, "");
+// const noZeros = arrayToString.replace(/^0+|0+$/g, "");
 
-console.log(noZeros);
+// console.log(noZeros);
 
-const backToNumber = Number(noZeros);
+// const backToNumber = Number(noZeros);
 
-const changeSignBack = () => {
-  if (number < 0) {
-    const finalResult = backToNumber * -1;
-    return finalResult;
-  } else {
-    const finalResult = backToNumber;
-    return finalResult;
-  }
-};
+// const changeSignBack = () => {
+//   if (number < 0) {
+//     const finalResult = backToNumber * -1;
+//     return finalResult;
+//   } else {
+//     const finalResult = backToNumber;
+//     return finalResult;
+//   }
+// };
 
-const finalResult = changeSignBack(backToNumber);
+// const finalResult = changeSignBack(backToNumber);
 
-console.log(finalResult);
+// console.log(finalResult);
