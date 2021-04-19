d8a0954aafc403a23c8860801d97aca1b3112bc2
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-19 13:21:23 -0500

    upload rest of morning notes

--- week7/day1/index.js
+++ week7/day1/index.js
@@ -1,15 +1,38 @@
-console.log("hi");
-
 const http = require("http");
+const express = require("express");
+const app = express();
 const address = "127.0.0.1";
 const PORT = 3002;
+const { readFile } = require("fs");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "text/plain");
-  res.end("hello");
+// app.use(express.json());
+
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-type", "text/plain");
+//   res.end("hello");
+// });
+
+// server.listen(PORT, address, () => {
+//   console.log(`server is on http://${address}:${PORT}`);
+// });
+
+app.get("/", (req, res) => {
+  readFile("index.html", "utf8", (err, html) => {
+    res.send(html);
+  });
 });
 
-server.listen(PORT, address, () => {
-  console.log(`server is on http://${address}:${PORT}`);
+app.get("/home", (req, res) => {
+  readFile("homepage.html", "utf8", (err, html) => {
+    res.send(html);
+  });
+});
+
+// app.get("/another-place", (req, res) => {
+//   //   res.end("Hello another-place");
+// });
+
+app.listen(PORT, () => {
+  console.log(`Your server is listening on ${PORT}`);
 });
