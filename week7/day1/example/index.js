97cfe04bf32681f56f13ada41dd624c8c2138854
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-19 15:21:11 -0500

    finishes afternoon exercises

--- week7/day1/example/index.js
+++ week7/day1/example/index.js
@@ -4,9 +4,8 @@ const { readFile } = require("fs");
 
 const PORT = 3003;
 
-app.listen(PORT, () => {
-  console.log(`Server is on localhost:${PORT}`);
-});
+// middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
   readFile("index.html", "utf8", (err, html) => {
@@ -26,8 +25,19 @@ app.get("/faq", (req, res) => {
   });
 });
 
+app.post("/messages", (req, res) => {
+  //   req.params;
+  const { name } = req.body;
+  const message = `Your name is ${name}`;
+  res.send(message);
+});
+
 app.get("*", (req, res) => {
   readFile("error.html", "utf8", (err, html) => {
     res.send(html);
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is on localhost:${PORT}`);
 });
