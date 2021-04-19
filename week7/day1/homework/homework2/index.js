c514ee48e2b2aa3b36aa92715f585ae2369c3bb8
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-19 16:48:15 -0500

    finishes styling link for homework

--- week7/day1/homework/homework2/index.js
+++ week7/day1/homework/homework2/index.js
@@ -3,12 +3,32 @@ const app = express();
 const PORT = 3005;
 const { readFile } = require("fs");
 
+app.use(express.static(__dirname + "/public"));
+
 app.listen(PORT, () => {
   console.log(`Server is on localhost:${PORT}`);
 });
 
 app.get("/", (req, res) => {
   readFile("index.html", "utf8", (err, html) => {
+    res.send(html);
+  });
+});
+
+app.get("/store", (req, res) => {
+  readFile("store.html", "utf8", (err, html) => {
+    res.send(html);
+  });
+});
+
+app.get("/contact", (req, res) => {
+  readFile("contact.html", "utf8", (err, html) => {
+    res.send(html);
+  });
+});
+
+app.get("*", (req, res) => {
+  readFile("errorpage.html", "utf8", (err, html) => {
     res.send(html);
   });
 });
