5f5f6fe8f1f8b50708cbe22750f1b837ca053b22
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-21 15:41:41 -0500

    Delete week7/day3 directory

--- week7/day3/db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE todolist;
-
-CREATE TABLE todo(
-    todo_id SERIAL PRIMARY KEY, -- will be created by the database on its own
-    description VARCHAR(255) -- needs this from user
-)
