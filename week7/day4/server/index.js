560439d3d0718eef377d51d569821aabf90646b8
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-22 13:13:07 -0500

    All CRUD functions working

--- week7/day4/server/index.js
+++ week7/day4/server/index.js
@@ -25,6 +25,55 @@ app.post("/vacation", async (req, res) => {
   }
 });
 
+app.get("/read_vacations", async (req, res) => {
+  try {
+    const readVacationsFromDB = await pool.query("SELECT * FROM locations");
+    res.json(readVacationsFromDB.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.get("/read_vacations/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const readSingleVacationFromDB = await pool.query(
+      "SELECT * FROM locations WHERE location_id = ($1)",
+      [id]
+    );
+    res.json(readSingleVacationFromDB);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.put("/update_vacation/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { location } = req.body;
+    const updateVacationInDB = await pool.query(
+      "UPDATE locations SET location = $1 WHERE location_id = $2",
+      [location, id]
+    );
+    res.json(updateVacationInDB);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.delete("/delete_vacation/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteVacationInDB = await pool.query(
+      "DELETE FROM locations WHERE location_id = $1",
+      [id]
+    );
+    res.json("Vacation location was successfully deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}.`);
 });
