1b7108bf326d6408d0b8704c5b9cbc6e386a1709
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-04-22 11:56:13 -0500

    create function working

--- week7/day4/server/index.js
+++ week7/day4/server/index.js
@@ -11,6 +11,20 @@ app.get("/", (req, res) => {
   res.send("Welcome to vacation list server");
 });
 
+app.post("/vacation", async (req, res) => {
+  try {
+    const { location } = req.body;
+
+    const newLocation = await pool.query(
+      "INSERT INTO locations (location) VALUES ($1)",
+      [location]
+    );
+    res.json(newLocation);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 app.listen(port, () => {
   console.log(`The server is listening on port ${port}.`);
 });
