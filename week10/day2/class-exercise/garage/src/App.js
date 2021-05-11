68d955d604800ac625cda08e963ca33add38fdb7
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-11 17:55:48 -0500

    removed commented-out code

--- week10/day2/class-exercise/garage/src/App.js
+++ week10/day2/class-exercise/garage/src/App.js
@@ -1,10 +1,6 @@
 import "./App.css";
 import React, { Component } from "react";
 import House from "./components/House";
-// import Garage from "./components/Garage";
-// import MuscleCarSection from "./components/MuscleCarSection";
-// import ExoticCarSection from "./components/ExoticCarSection";
-// import MotorcycleSection from "./components/MotorcycleSection";
 
 export default class App extends Component {
   render() {
