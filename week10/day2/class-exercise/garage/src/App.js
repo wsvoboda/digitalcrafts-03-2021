cfdc6fe9b74c669688fcf414a3013b116bf13c49
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-11 13:53:30 -0500

    creates components and main class

--- week10/day2/class-exercise/garage/src/App.js
+++ week10/day2/class-exercise/garage/src/App.js
@@ -1,11 +1,58 @@
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vehicles: [
+        {
+          motorcycles: [
+            {
+              american: [
+                { name: "Harley" },
+                { name: "Indian" },
+                { name: "Victory" },
+              ],
+            },
+            {
+              foreign: [
+                { name: "Kawasaki" },
+                { name: "Honda" },
+                { name: "Ducati" },
+              ],
+            },
+          ],
+        },
+        {
+          exotic: [
+            {
+              american: [
+                { name: "Dodge" },
+                { name: "Chevrolet" },
+                { name: "Ford" },
+              ],
+            },
+            {
+              foreign: [{ name: "BMW" }, { name: "Audi" }, { name: "Porsche" }],
+            },
+          ],
+        },
+        {
+          muscle: [
+            {
+              american: [{ name: "Black" }, { name: "Blue" }, { name: "Red" }],
+            },
+            {
+              foreign: [{ name: "Noir" }, { name: "Bleue" }, { name: "Rouge" }],
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  render() {
+    return <div></div>;
+  }
 }
-
-export default App;
