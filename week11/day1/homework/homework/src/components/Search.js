e4c35a3cbb055d8ae41a29dcf22f1beaad283ae8
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-17 17:21:38 -0500

    centered search bar

--- week11/day1/homework/homework/src/components/Search.js
+++ week11/day1/homework/homework/src/components/Search.js
@@ -4,9 +4,7 @@ export default class Search extends Component {
   render() {
     return (
       <div className="search">
-        <div className="search-bar">
-          <input className="input-bar" type="text"></input>
-        </div>
+        <input className="input-bar" type="text"></input>
       </div>
     );
   }
