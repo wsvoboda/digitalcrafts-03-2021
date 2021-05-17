b06a3f66424b2b673a56ccde15cd5957272ae7e6
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-17 10:12:28 -0500

    creates react app for morning exercise

---
+++ week11/day1/morning-exercise/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Class from "./Class.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App.js</h1>
+      <Class />
+    </div>
+  );
+}
+
+export default App;
---
+++ week11/day1/morning-exercise/src/Class.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Class extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Class.js</h1>
+      </div>
+    );
+  }
+}
