a6dadd56a16c4d33b975d026c37bf7e562138bab
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-18 11:15:14 -0500

    search functioning

--- week11/day2/props-passing/src/components/CardContainer.jsx
+++ week11/day2/props-passing/src/components/CardContainer.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 import Card from "./Card"
 
 export default class CardContainer extends Component {
+    state = {
+        searchCriteria: "" 
+    }
+
+    searchCriteria = (event) => {
+        this.setState({searchCriteria: event.target.value})
+    }
     render() {
-        console.log(this.props.pokemonData)
         const {pokemonData} = this.props
+        const filteredData = pokemonData[0].pokemon.filter(pokemon => pokemon.name.includes(this.state.searchCriteria))
         return (
             <div>
                 <h1>Card Container</h1>
-                {pokemonData[0].pokemon.map((singlePokemon,index) => <Card key={singlePokemon.name} pokemon={singlePokemon}/>)} 
+                <input onChange={(e)=>this.searchCriteria(e)} type="text" placeholder="Search for a Pokemon"/>
+                <div className="card-container">
+                {filteredData.map((singlePokemon,index) => <Card key={singlePokemon.name} pokemon={singlePokemon}/>)} </div>
             </div>
         )
     }
