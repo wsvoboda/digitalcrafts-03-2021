76d7cc0d07931c7d59e36478c92f0ef2ebce6c3f
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-18 16:44:07 -0500

    imported info from class

--- week11/day2/props-passing/src/components/CardContainer.jsx
+++ week11/day2/props-passing/src/components/CardContainer.jsx
@@ -1,46 +1,123 @@
-import React, { Component } from 'react'
-import Card from "./Card"
-import "./cardContainer.css"
+import React, { Component } from "react";
+import Card from "./Card";
+import "./cardContainer.css";
 
-export default class CardContainer extends Component {
-    state = {
-        searchCriteria: "" ,
-        pokemonBerries: []
-    }
+class CardContainer extends Component {
+  state = {
+    searchCriteria: "",
+    pokemonList: [],
+    pokemonBerries: [],
+    pokemonName: "",
+    hp: "",
+    frontUrl: "",
+    backUrl: "",
+  };
 
-    searchCriteria = (event) => {
-        const search = event.target.value.toLowerCase()
+ componentDidMount() {
+    this.setState({
+      pokemonList: this.props.pokemonData[0].pokemon,
+    });
+  }
 
-        this.setState({searchCriteria: search})
-    }
-    async componentDidMount() {
-        const berryURL = "https://pokeapi.co/api/v2/berry/"
-        const fetchData = await fetch(berryURL, {headers: {
-        "content-type": "application/json"},})
-    const json = await fetchData.json()
-    this.setState({pokemonBerries: json.results})
-}
-    render() {
-        const {pokemonData} = this.props
-        const filteredData = pokemonData[0].pokemon.filter(pokemon => pokemon.name.includes(this.state.searchCriteria)) ? pokemonData[0].pokemon.filter(pokemon => pokemon.name.includes(this.state.searchCriteria)) :pokemonData[0].pokemon
-        return (
-            <div className="main-card-container">
-                <div>
-                <input onChange={(e)=>this.searchCriteria(e)} type="text" placeholder="Search for a Pokemon"/>
+  searchCriteria = (event) => {
+    const search = event.target.value.toLowerCase();
+    this.setState({
+      searchCriteria: search,
+    });
+  };
 
-                </div>
-                <div>
-                 <input type="text" placeholder="Enter a Name"/>
-                 <input  type="text" placeholder="Enter an HP"/>
-                <button>Create</button>
-                </div>
-                <div className="card-container">
+  sendPokemonToData = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    e.preventDefault();
+  };
 
-                {filteredData.map((singlePokemon,index) => <Card key={singlePokemon.name}  pokemon={singlePokemon}  />
-                )}
-                </div>
-              
-            </div>
+  onSubmit = (event, filteredData) => {
+    event.preventDefault();
+    const newPokemon = {
+      name: this.state.pokemonName,
+      hp: this.state.hp,
+      sprites: {
+        front: this.state.frontUrl,
+        back: this.state.backUrl,
+      },
+    };
+    
+    const newPokemonList = [newPokemon,...this.state.pokemonList]
+    this.setState({
+      pokemonList: newPokemonList,
+    });
+  };
+
+  render() {
+    const {pokemonList} = this.state
+    const filteredData = pokemonList.filter((pokemon) =>
+      pokemon.name.includes(this.state.searchCriteria)
+    )
+      ? pokemonList.filter((pokemon) =>
+          pokemon.name.includes(this.state.searchCriteria)
         )
-    }
+      : pokemonList
+
+    return (
+      <div className="main-card-container">
+        <div>
+          <input
+            name="pokemonName"
+            className="search-field"
+            onChange={(e) => this.searchCriteria(e)}
+            type="text"
+            placeholder="Search for a pokemon"
+          />
+        </div>
+        {/* form */}
+        <div>
+          
+          <form onSubmit={(e) => this.onSubmit(e, filteredData)}>
+            <input
+              name="pokemonName"
+              className="create-field"
+              type="text"
+              placeholder="Enter a Name"
+              value={this.state.pokemonName}
+              onChange={(e) => this.sendPokemonToData(e)}
+            />
+            <input
+              name="hp"
+              className="create-field"
+              type="text"
+              placeholder="Enter a HP"
+              value={this.state.hp}
+              onChange={(e) => this.sendPokemonToData(e)}
+            />
+            <input
+              name="frontUrl"
+              className="create-field"
+              type="text"
+              placeholder="Enter front URL"
+              value={this.state.frontUrl}
+              onChange={(e) => this.sendPokemonToData(e)}
+            />
+            <input
+              name="backUrl"
+              className="create-field"
+              type="text"
+              placeholder="Enter back URL"
+              value={this.state.backUrl}
+              onChange={(e) => this.sendPokemonToData(e)}
+            />
+            <input className="create-field" type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="card-container">
+          {filteredData.map((singlePokemon, index) => (
+            <Card key={singlePokemon.name} pokemon={singlePokemon} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default CardContainer;
