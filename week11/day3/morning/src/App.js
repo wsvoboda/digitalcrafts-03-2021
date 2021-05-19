b3d815114e632afc5ccbddd9251bf780e11d98da
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-19 09:39:15 -0500

    change css

--- week11/day3/morning/src/App.js
+++ week11/day3/morning/src/App.js
@@ -3,19 +3,59 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Forms React</h1>
+      <h1 className="shipping-form-header">Forms React</h1>
       <div className="shipping-form-container">
         <form className="shipping-form">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="text" placeholder="Street Address" />
-          <input type="text" placeholder="Apt, Ste" />
-          <input type="text" placeholder="Postal Code" />
-          <input type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
-          <input type="text" placeholder="Country" />
-          <input type="text" placeholder="Phone" />
-          <input type="submit" placeholder="Submit" />
+          <input
+            className="shipping-input-first-name"
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            className="shipping-input-last-name"
+            type="text"
+            placeholder="Last Name"
+          />
+          <input
+            className="shipping-input-address"
+            type="text"
+            placeholder="Street Address"
+          />
+          <input
+            className="shipping-input-apt"
+            type="text"
+            placeholder="Apt, Ste"
+          />
+          <input
+            className="shipping-input-zip"
+            type="text"
+            placeholder="Postal Code"
+          />
+          <input
+            className="shipping-input-city"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            className="shipping-input-state"
+            type="text"
+            placeholder="State"
+          />
+          <input
+            className="shipping-input-country"
+            type="text"
+            placeholder="Country"
+          />
+          <input
+            className="shipping-input-phone"
+            type="text"
+            placeholder="Phone"
+          />
+          <input
+            className="shipping-input-submit"
+            type="submit"
+            placeholder="Submit"
+          />
         </form>
       </div>
     </div>
