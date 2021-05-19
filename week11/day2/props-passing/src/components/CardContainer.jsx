9955afd1feb04613da72d0aa9005fea55f652b1a
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-19 17:55:30 -0500

    search and create for berries functioning

--- week11/day2/props-passing/src/components/CardContainer.jsx
+++ week11/day2/props-passing/src/components/CardContainer.jsx
@@ -5,9 +5,11 @@ import "./cardContainer.css";
 
 class CardContainer extends Component {
   state = {
-    searchCriteria: "",
+    pokemonSearchCriteria: "",
+    berrySearchCriteria: "",
     pokemonList: [],
     pokemonBerries: [],
+    name: "",
     pokemonName: "",
     hp: "",
     frontUrl: "",
@@ -19,21 +21,26 @@ class CardContainer extends Component {
   const fetchData = await fetch(berryUrl,{ headers: {
  'Content-Type': 'application/json'
 },})
-const json = await fetchData.json()
+  const json = await fetchData.json()
     this.setState({
       pokemonList: this.props.pokemonData[0].pokemon,
-      pokemonBerries:json.results
+      pokemonBerries:json.results,
     });
   }
 
- 
-
-  searchCriteria = (e) => {
-    const search = e.target.value.toLowerCase();
+  pokemonSearchCriteria = (e) => {
+    const pokemonSearch = e.target.value.toLowerCase();
     this.setState({
-      searchCriteria: search,
+      pokemonSearchCriteria: pokemonSearch,
     });
   };
+
+  berrySearchCriteria = (e) => {
+    const berrySearch = e.target.value.toLowerCase();
+    this.setState({
+      berrySearchCriteria: berrySearch,
+    })
+  }
 
   sendPokemonToData = (e) => {
     this.setState({
@@ -42,49 +49,77 @@ const json = await fetchData.json()
     e.preventDefault();
   };
 
-  onSubmit = (e, filteredData) => {
+  sendBerryToData = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    e.preventDefault();
+  };
+
+  onPokemonSubmit = (e, filteredPokemonData) => {
     e.preventDefault();
     const newPokemon = {
       name: this.state.pokemonName,
-
       hp: this.state.hp,
       sprites: {
         front: this.state.frontUrl,
         back: this.state.backUrl,
       },
     };
-    
     const newPokemonList = [newPokemon,...this.state.pokemonList]
     this.setState({
       pokemonList: newPokemonList,
     });
   };
 
+  onBerrySubmit = (e, filteredBerryData) => {
+      e.preventDefault();
+      const newBerry = {
+        name: this.state.name,
+      };
+      const newBerryList = [newBerry,...this.state.pokemonBerries]
+      this.setState({
+        pokemonBerries: newBerryList,
+      });
+    };
+    
+    
   render() {
     const {pokemonList} = this.state
     const {pokemonBerries} = this.state
-    const filteredData = pokemonList.filter((pokemon) =>
-      pokemon.name.includes(this.state.searchCriteria)
+    const filteredPokemonData = (pokemonList.filter((pokemon) =>
+      pokemon.name.includes(this.state.pokemonSearchCriteria)
     )
       ? pokemonList.filter((pokemon) =>
-          pokemon.name.includes(this.state.searchCriteria)
+          pokemon.name.includes(this.state.pokemonSearchCriteria)
         )
-      : pokemonList
+      : pokemonList)
+      
+    const filteredBerryData = (pokemonBerries.filter((berry) =>
+      berry.name.includes(this.state.berrySearchCriteria)
+    )
+      ? pokemonBerries.filter((berry) =>
+          berry.name.includes(this.state.berrySearchCriteria)
+        )
+      : pokemonBerries)
+
     return (
       <div className="main-card-container">
-        <div>
+        
+        <div className="headername">
+          <h1>Pokemon</h1>
+        </div>
+        <div className="searchbar">
           <input
             name="name"
             className="search-field"
-            onChange={this.searchCriteria}
+            onChange={this.pokemonSearchCriteria}
             type="text"
             placeholder="Search for a Pokemon"
           />
         </div>
-        <div className="headername">
-          <h1>Pokemon</h1></div>
-          <div>
-          <form onSubmit={this.onSubmit}>
+        <div class="formdiv">
+          <form onSubmit={this.onPokemonSubmit}>
             <input
               name="pokemonName"
               className="create-field"
@@ -120,18 +155,44 @@ const json = await fetchData.json()
             <input className="create-field" type="submit" value="Submit" />
           </form>
         </div>
+
         <div className="card-container">
-          
-          {filteredData.map((singlePokemon, index) => (
+          {filteredPokemonData.map((singlePokemon, index) => (
             <Card key={singlePokemon.name} pokemon={singlePokemon} />
-            ))}</div><div className="headername">
-            <h1>Berries</h1></div>
-            <div className="card-container">
-              
-          {pokemonBerries.map((singleBerry, index) => (
+            ))}
+        </div>
+
+            <div className="headername">
+            <h1>Berries</h1>
+            </div>
+            
+            <div className="searchbar">
+            <input
+            name="name"
+            className="search-field"
+            onChange={this.berrySearchCriteria}
+            type="text"
+            placeholder="Search for a Berry"
+            />
+            </div>
+            <div class="formdiv">
+          <form onSubmit={this.onBerrySubmit}>
+            <input
+              name="name"
+              className="create-field"
+              type="text"
+              placeholder="Enter a Berry Name"
+              value={this.state.name}
+              onChange={this.sendBerryToData}
+            />
+            <input className="create-field" type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="card-container">
+          {filteredBerryData.map((singleBerry, index) => (
           <Berries key={singleBerry.name} berry={singleBerry}/>
           ))}
-        </div>
+          </div>
         </div>
     );
   }
