557774b49660d76a76ade30d9cdf5eb226ef39f5
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-25 11:27:52 -0500

    create routes and make error page

--- week12/day2/morning-exercise/src/App.js
+++ week12/day2/morning-exercise/src/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
+import AboutMe from "./components/AboutMe";
+import Blog from "./components/Blog";
+import Home from "./components/Home";
+import ErrorPage from "./components/ErrorPage";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Routes</h1>
+      <Router>
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/blog">Blog</Link>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/aboutme">
+            <AboutMe />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
