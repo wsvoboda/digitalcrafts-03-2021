770895701592cb7f2f119ea2b4f9fcd4c550a4ca
Whitney Svoboda <78281930+wsvoboda@users.noreply.github.com>
2021-05-25 09:03:37 -0500

    created new react app for morning

--- week12/day1/movie-site/src/components/AllSearchedMovies.js
+++ week12/day1/movie-site/src/components/AllSearchedMovies.js
@@ -1,14 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "../App.css";
-import genericPhoto from "../images/movie.png";
+// import genericPhoto from "../images/movie.png";
 
 export default function AllSearchedMovies(props) {
-  //   const [moviePoster, setMoviePoster] = useState("");
-  //   if (props.movie.Poster === "N/A") {
-  //     setMoviePoster({ genericPhoto });
-  //   } else {
-  //     setMoviePoster(props.movie.Poster);
-  //   }
   return (
     <div className="all-movies">
       <div className="one-movie">
